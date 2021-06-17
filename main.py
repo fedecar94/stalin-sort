8d90b42178eb04bcbece8cec6654995bfdf28e3c
Federico Cardozo <fedecar94@outlook.com>
2021-06-16 21:51:49 -0400

    minimun change

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ def stalin_sort(array: list):
     if not len(array):
         return []
     pivot = array[0]
-    sorted_array = []
+    new_array = []
     for element in array:
         print(f'I am {element}')
         try:
             if pivot <= element:
-                sorted_array.append(element)
+                new_array.append(element)
                 pivot = element
             else:
                 print(f'{element} 🔫 Not in order!')
         except Exception as e:
-            print(f'{element} 🔫 Not sortable!')
-    return sorted_array
+            print(f'{element} 🔫 Not sortable!', e)
+
+    return new_array
 
 
 if __name__ == '__main__':
     import random
-    randomlist = []
-    for i in range(0, 10):
-        n = random.randint(1, 30)
-        randomlist.append(n)
-    print(stalin_sort(randomlist))
+
+    randomlist = [random.randint(1, 30) for x in range(0, 10)]
+
+    print(randomlist)
+    sorted_array = stalin_sort(randomlist)
+    print(sorted_array)
